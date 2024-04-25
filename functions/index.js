3d8b787e74a4261e81e47f718ece28f239eed3b1
Pranav Ahuja <72250259+kingpranav21@users.noreply.github.com>
2024-04-25 15:12:16 +0530

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,7 @@ const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
 const stripe = require("stripe")(
-  "sk_test_51HPvU9DFg5koCdLGeOEiFvwHat4v8eMjX6SY0YCwxPBQBUPhKy1fPVhiSM5cQtgW7QBG9ydQcXnW57TDxVE2f3H000HSfmEQZF"
+  "process.env.STRIPE_API_TEST_URI"
 );
 
 // API
